aa8988610f0012f1c00ebd709e697916602c4a10
Bagaskrunia <69422694+Bagaskrunia@users.noreply.github.com>
2021-04-04 10:49:03 -0700

    oiii
    oii

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+$(".slider").slick({
+  // normal options...
+  infinite: true,
+  autoplay: true,
+  nav: true,
+  autoplayHoverPause: true,
+  arrows: false,
+  slidesToScroll: 3,
+  slidesToShow: 3,
+  loop: true,
+  // the magic
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        infinite: true,
+        loop: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        infinite: true,
+        slidesToShow: 2,
+        loop: true,
+      },
+    },
+    {
+      breakpoint: 300,
+      settings: {
+        infinite: true,
+        slidesToShow: 2,
+        loop: true,
+      }, // destroys slick
+    },
+  ],
+});
